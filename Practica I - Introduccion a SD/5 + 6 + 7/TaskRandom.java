98ba99112e2dc658d182b14c29defc20de17295c
Nehuen Prados <nehuensd@gmail.com>
2020-03-21 15:35:57 -0300

    Update!

--- Practica I - Introduccion a SD/5 + 6 + 7/TaskRandom.java
+++ Practica I - Introduccion a SD/5 + 6 + 7/TaskRandom.java
@@ -1,7 +1,35 @@
 package sdypp;
 
-import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Random;
 
-public interface RemoteWorker extends Remote {
-	public String getClima () throws java.rmi.RemoteException;
+public class TaskRandom extends UnicastRemoteObject implements Task {
+
+  private static final long serialVersionUID = 1L;
+  
+  protected int min;
+  protected int max;
+  protected int val;
+
+  protected TaskRandom() throws RemoteException {
+    super();
+  }
+  
+  public void setMin(int _min) {
+    min = _min;
+  }
+  
+  public void setMax(int _max) {
+    max = _max;
+  }
+  
+  public int getVal() {
+    return val;
+  }
+  
+  public void run() {
+    Random random = new Random();
+    val = random.nextInt(max - min) + min;
+  }
 }
